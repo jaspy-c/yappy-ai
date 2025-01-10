9a73abf0bd43a7840c6a064b248661e1b09da37b
jaspy-c <jaychen@ucdavis.edu>
2025-01-09 18:39:30 -0800

    Updated Contact Email

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -4,7 +4,7 @@ export const ContactSection = () => {
   return (
     <div className='flex flex-col items-center justify-between text-center'>
       <div className='text-6xl font-bold p-3 tracking-tight text-balance'>Contact Us</div>
-      <div className='text-lg font-semibold text-balance'>support@yappy.ai</div>
+      <div className='text-lg font-semibold text-balance'>support@yappy-ai.com</div>
     </div>
   )
 }
